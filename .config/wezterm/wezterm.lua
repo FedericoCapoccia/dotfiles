4a103da1de4d1c4b43043ca3f43ac8080c928756
Federico Capoccia <federico.capoccia@proton.me>
2024-06-29 12:58:08 +0200

    Use a real JS construct, WTF knows why this works in chromium.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,4 +2,7 @@ local wezterm = require 'wezterm'
 local config = {}
 
 config.font = wezterm.font 'JetBrainsMono NF Medium'
+--config.color_scheme = 'Breeze'
+config.window_close_confirmation = 'NeverPrompt'
+
 return config
