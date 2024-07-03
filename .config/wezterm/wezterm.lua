48b2ab3645611adff66d451c4a7121ac0076e726
Federico Capoccia <166076838+FedericoCapoccia@users.noreply.github.com>
2024-07-03 18:08:06 +0200

    Don’t mess with Voodoo

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm'
 local config = {}
 
 config.font = wezterm.font 'JetBrainsMono NF Medium'
---config.color_scheme = 'Breeze'
+config.color_scheme = 'MaterialOcean'
 config.window_close_confirmation = 'NeverPrompt'
 
 return config
