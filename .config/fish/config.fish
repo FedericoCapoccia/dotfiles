3b5053f2825b34ab80f50bf7b57de4e6521d8bc3
Federico Capoccia <166076838+FedericoCapoccia@users.noreply.github.com>
2026-01-19 13:16:13 +0100

    needs more cow bell

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,24 +1,21 @@
-if status --is-login
-    # XDG vars
-    set -gx XDG_CONFIG_HOME "$HOME/.config"
-    set -gx XDG_CACHE_HOME "$HOME/.cache"
-    set -gx XDG_DATA_HOME "$HOME/.local/share"
-    set -gx XDG_STATE_HOME "$HOME/.local/state"
+# XDG vars
+set -gx XDG_CONFIG_HOME "$HOME/.config"
+set -gx XDG_CACHE_HOME "$HOME/.cache"
+set -gx XDG_DATA_HOME "$HOME/.local/share"
+set -gx XDG_STATE_HOME "$HOME/.local/state"
 
-    # Other env
-    set -gx DOTNET_CLI_TELEMETRY_OPTOUT "1"
-    set -gx EDITOR "nvim"
+# Other env
+set -gx DOTNET_CLI_TELEMETRY_OPTOUT "1"
+set -gx EDITOR "nvim"
 
-    # Git prompt stuff
-    set -gx __fish_git_prompt_showdirtystate true
+# PATH
+fish_add_path "$HOME/.local/bin"
+fish_add_path "$HOME/.cargo/bin"
+fish_add_path "$HOME/.zvm/bin"
 
-    fish_add_path "$HOME/.local/bin"
-    fish_add_path "$HOME/.cargo/bin"
-    fish_add_path "$HOME/.zvm/bin"
+if test -n "$SSH_CLIENT"
+    set -gx TERM xterm
 end
 
-if status --is-interactive
-    if test -n "$SSH_CLIENT"
-        set -gx TERM xterm
-    end
-end
+# Git prompt stuff
+set -gx __fish_git_prompt_showdirtystate true
