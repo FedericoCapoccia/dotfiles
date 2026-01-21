34385fb045f8e1afc15779bfdb34e12774ebbdbb
Federico Capoccia <166076838+FedericoCapoccia@users.noreply.github.com>
2026-01-21 18:53:55 +0100

    this is Spartaaaaaaaa

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,6 +7,7 @@ set -gx XDG_STATE_HOME "$HOME/.local/state"
 # Other env
 set -gx DOTNET_CLI_TELEMETRY_OPTOUT "1"
 set -gx EDITOR "nvim"
+set -gx SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/gcr/ssh"
 
 # PATH
 fish_add_path "$HOME/.local/bin"
