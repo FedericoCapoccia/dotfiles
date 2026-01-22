8b0ad7ee32b1fce6532786fc6f95a265d3fff329
Federico Capoccia <166076838+FedericoCapoccia@users.noreply.github.com>
2026-01-22 23:42:57 +0100

    It worked for me...

--- .config/fish/conf.d/sway.fish
+++ .config/fish/conf.d/sway.fish
@@ -1,5 +1,7 @@
 # If running from tty1 start sway
-set -gx XDG_CURRENT_DESKTOP "sway:wlroots"
-set -gx WLR_RENDERER "vulkan"
-set TTY1 (tty)
-[ "$TTY1" = "/dev/tty1" ] && exec dbus-run-session sway
+if test (hostname) = "andromeda"
+    set -gx XDG_CURRENT_DESKTOP "sway:wlroots"
+    set -gx WLR_RENDERER "vulkan"
+    set TTY1 (tty)
+    [ "$TTY1" = "/dev/tty1" ] && exec dbus-run-session sway
+end
